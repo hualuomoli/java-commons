5a31a0df34d8e822a3ac9a7da2dcf96e9e0d1d39
hualuomoli <liubaoquan0831@163.com>
2016-07-03 16:16:00 +0800

    add date format

--- src/main/java/com/github/hualuomoli/constant/DateFormat.java
+++ src/main/java/com/github/hualuomoli/constant/DateFormat.java
@@ -9,11 +9,18 @@ import java.text.SimpleDateFormat;
  */
 public class DateFormat {
 
-	public static final SimpleDateFormat SDF_SLASHYMD = new SimpleDateFormat("yyyy/MM/dd");
-	public static final SimpleDateFormat SDF_SLASHYMD_HMS = new SimpleDateFormat("yyyy/MM/dd kk:mm:ss");
-	public static final SimpleDateFormat SDF_BARYMD = new SimpleDateFormat("yyyy-MM-dd");
-	public static final SimpleDateFormat SDF_BARYMD_HMS = new SimpleDateFormat("yyyy-MM-dd kk:mm:ss");
-	public static final SimpleDateFormat SDF_YMD = new SimpleDateFormat("yyyyMMdd");
-	public static final SimpleDateFormat SDF_YMD_HMS = new SimpleDateFormat("yyyyMMddHHmmss");
+	public static final String PATTERN_SLASHYMD = "yyyy/MM/dd";
+	public static final String PATTERN_SLASHYMD_HMS = "yyyy/MM/dd kk:mm:ss";
+	public static final String PATTERN_BARYMD = "yyyy-MM-dd";
+	public static final String PATTERN_BARYMD_HMS = "yyyy-MM-dd kk:mm:ss";
+	public static final String PATTERN_YMD = "yyyyMMdd";
+	public static final String PATTERN_YMD_HMS = "yyyyMMddHHmmss";
+
+	public static final SimpleDateFormat SDF_SLASHYMD = new SimpleDateFormat(PATTERN_SLASHYMD);
+	public static final SimpleDateFormat SDF_SLASHYMD_HMS = new SimpleDateFormat(PATTERN_SLASHYMD_HMS);
+	public static final SimpleDateFormat SDF_BARYMD = new SimpleDateFormat(PATTERN_BARYMD);
+	public static final SimpleDateFormat SDF_BARYMD_HMS = new SimpleDateFormat(PATTERN_BARYMD_HMS);
+	public static final SimpleDateFormat SDF_YMD = new SimpleDateFormat(PATTERN_YMD);
+	public static final SimpleDateFormat SDF_YMD_HMS = new SimpleDateFormat(PATTERN_YMD_HMS);
 
 }
